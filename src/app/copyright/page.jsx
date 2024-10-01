19e90c16b12c00acc1e2af6cc62e48a55cf04f4d
FardusWahid <farduswahid87@gmail.com>
2024-10-01 14:32:18 -0700

    newcommit

--- src/app/copyright/page.jsx
+++ src/app/copyright/page.jsx
@@ -7,8 +7,8 @@ function Copy() {
 <div className='w-11/12 md:w-2/5 m-auto bg-gray-900 rounded-lg ring-1 ring-purple-950 shadow-xl shadow-gray-800 pt-3 pb-10 px-5'>
 
 <p className='justify-start font-extrabold text-xl tracking-wide hover:text-2xl selection:bg-none selection:text-yellow-300'>Copyright!</p>
-<p className='mt-2 font-sans font-semibold text-[16px] hover:text-xl hover:font-extrabold selection:bg-none selection:text-purple-600'>AppleCourses does not pirates any of the courses included in this application, all the video links are from third party websites & channels available freely on the internet. </p>
-<p className='mt-2 font-sans font-semibold text-[16px] hover:text-xl hover:font-extrabold selection:bg-none selection:text-purple-600'>We are just giving these way for a very small amount of maintenance fee. All the content is the copyright of their respective owner!</p>
+<p className='mt-2 font-sans font-semibold text-[16px] hover:text-xl hover:font-extrabold selection:bg-none selection:text-purple-600'>AppleCourses does not pirates any of the course included in this application, all the video links are from third party websites & channels available freely on the internet. </p>
+<p className='mt-2 font-sans font-semibold text-[16px] hover:text-xl hover:font-extrabold selection:bg-none selection:text-purple-600'>we are just giving these way for a very small amount of maintenance fee. All the content is copyright property of their respective owner!</p>
 </div>
 
 
